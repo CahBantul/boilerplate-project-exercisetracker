792fcee6b5762a4faef40ab7528b402b41e17197
Fardan Nozami <fardan.nozami@gmail.com>
2022-05-26 20:04:58 +0700

    add model Exercise

---
+++ models/Exercise.js
@@ -0,0 +1,23 @@
+const { Schema } = require('mongoose');
+
+const exerciseSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
